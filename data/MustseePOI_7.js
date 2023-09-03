46edd0e125bc85b00d24dcbe1d69d37083e3e375
Nicko05 <55138611+Nicko05@users.noreply.github.com>
2023-09-03 17:46:32 +0200

    Add files via upload

---
+++ data/MustseePOI_7.js
@@ -0,0 +1,19 @@
+var json_MustseePOI_7 = {
+"type": "FeatureCollection",
+"name": "MustseePOI_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Opis/Description": "Ivanečki bajeri s sjedištem ŠRD", "Koordinate/Coordinates": "46.23009027, 16.11135116", "Nadmorska visina/Altitude": "210" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.111348479159226, 46.230090837679825 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Panoramski pogled Gačice", "Koordinate/Coordinates": "46.22897672, 16.19862187", "Nadmorska visina/Altitude": "291" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.198620839561382, 46.228977007016312 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Muzej Planinarstva", "Koordinate/Coordinates": "46.22158337, 16.12548184", "Nadmorska visina/Altitude": "230" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.125480399435038, 46.221583087656342 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Friščićev Mlin", "Koordinate/Coordinates": "46.20049138, 16.13414309", "Nadmorska visina/Altitude": "372" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.134141232940443, 46.200491948020868 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Vodenica u Margečanu", "Koordinate/Coordinates": "46.21482855, 16.22741937", "Nadmorska visina/Altitude": "195" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.227418337132814, 46.214827410346132 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Kapela sveti Duh u Prigorcu", "Koordinate/Coordinates": "46.20591834, 16.13813876", "Nadmorska visina/Altitude": "449" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.138135255430864, 46.205919200059952 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Panoramski pogled s parkinga u Prigorcu", "Koordinate/Coordinates": "46.19696219, 16.13879552", "Nadmorska visina/Altitude": "437" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.138792834696108, 46.196963615587002 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Bedenska hiža", "Koordinate/Coordinates": "46.23880966, 16.06301262", "Nadmorska visina/Altitude": "243" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.063011176713687, 46.238809664229521 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Centar za posjetitelje", "Koordinate/Coordinates": "46.224219870871636, 16.124588679766333", "Nadmorska visina/Altitude": "225" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.124590606546668, 46.224219870871629 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Ivanečka jezera", "Koordinate/Coordinates": "46.22414768928626, 16.133223548057604", "Nadmorska visina/Altitude": "217" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.133224610195267, 46.224146219629901 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Vražji stolček", "Koordinate/Coordinates": "46.22082174, 16.20928200", "Nadmorska visina/Altitude": "302" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.209305121830294, 46.220805742765961 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Rodna kuća narodnog heroja Josipa Kraša", "Koordinate/Coordinates": "46.21625403, 16.09789476", "Nadmorska visina/Altitude": "264" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.09788704879535, 46.216243366259746 ] ] } }
+]
+}
---
+++ data/SakralnabatinaSacralheritage_12.js
@@ -0,0 +1,14 @@
+var json_SakralnabatinaSacralheritage_12 = {
+"type": "FeatureCollection",
+"name": "SakralnabatinaSacralheritage_12",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Naziv/Name": "Župna crkva Sv. Marije Magdalene", "Adresa/Adress": "Rudolfa Rajtera 1, Ivanec", "Koordinate/Coordinates": "46.22292222, 16.12505104", "Nadmorska visina/Altitude": "229" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.125062595810782, 46.222920889092457 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Crkva sv. Margarete", "Adresa/Adress": "Margečan 24A, 42242, Margečan", "Koordinate/Coordinates": "46.21436251052133, 16.22572264122982", "Nadmorska visina/Altitude": "198" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.225722641229819, 46.214367843557909 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Crkva Blažene Djevice Marije", "Adresa/Adress": "Varaždinska ul. 18, Radovan", "Koordinate/Coordinates": "46.2313743055127, 16.24710833147635", "Nadmorska visina/Altitude": "203" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.247110258256686, 46.231372972666541 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Kapela Sveti Duh", "Adresa/Adress": "Prigorec ul. 83E, Prigorec", "Koordinate/Coordinates": "46.20584037, 16.13802366", "Nadmorska visina/Altitude": "450" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.138019809130128, 46.205832365579646 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Kapela sv. Donata", "Adresa/Adress": "Ul. Vladimira Nazora 56, Ivanec", "Koordinate/Coordinates": "46.22877845960217, 16.123956260233204", "Nadmorska visina/Altitude": "216" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.124036221617192, 46.22870781536998 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Samostan Marije Suotkupiteljice", "Adresa/Adress": "Ivanečki Vrhovec 36a", "Koordinate/Coordinates": "46.23294574658147, 16.15601816682269", "Nadmorska visina/Altitude": "234" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.155929534927182, 46.232884437379973 ] ] } },
+{ "type": "Feature", "properties": { "Naziv/Name": "Kapela Sv. Ivana Krstitelja", "Adresa/Adress": null, "Koordinate/Coordinates": "46.18189797633319, 16.123817499863915", "Nadmorska visina/Altitude": "1030" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.123821353424589, 46.181899310380338 ] ] } }
+]
+}
